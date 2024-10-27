6a62fe34d11183b46153037b2a0a0fa1844762f4
Thanos Antonopoulos <78595164+thanantonop@users.noreply.github.com>
2024-10-27 13:42:52 +0200

    ubuntu-lts
    ubuntu-lts

--- ubuntu.sh
+++ ubuntu.sh
@@ -4,14 +4,20 @@ sudo apt autoremove -y
 sudo apt autopurge -y
 sudo apt autoclean -y
 
-sudo apt install build-essential automake autoconf git vim mc htop nmap neofetch gnome-weather gnome-tweaks gnome-shell-extension-manager gdebi synaptic libfuse2t64 ubuntu-restricted-extras ttf-mscorefonts-installer curl linux-headers-$(uname -r) linux-headers-generic unzip p7zip bzip2 gcc make tar wget unrar default-jdk bpytop flatpak gnome-software gnome-software-plugin-flatpak -y
+sudo apt install build-essential automake autoconf git vim mc htop nmap neofetch gufw gnome-weather gnome-tweaks gnome-shell-extension-manager gdebi synaptic libfuse2t64 ubuntu-restricted-extras ttf-mscorefonts-installer curl linux-headers-$(uname -r) linux-headers-generic unzip p7zip bzip2 gcc make tar wget unrar default-jdk bpytop flatpak gnome-software gnome-software-plugin-flatpak -y
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+sudo ufw enable
 
 sudo snap refresh
-sudo snap install gimp
+sudo snap install brave
 sudo snap install keepassxc
 sudo snap install vlc
+sudo snap install gimp
+sudo snap install inkscape
+sudo snap install krita
 sudo snap install darktable
+sudo snap install spotify
+sudo snap install rnote
 sudo snap install onlyoffice-desktopeditors
 sudo snap install code --classic
 sudo snap install sublime-text --classic
